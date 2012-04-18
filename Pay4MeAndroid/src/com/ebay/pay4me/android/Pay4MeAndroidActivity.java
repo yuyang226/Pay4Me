afab9e07b4e93424c052c87d22f7e674839673f2
Toby Yu <yayu@ebay.com>
2012-04-18 20:28:55 +0800

    adding android project

---
+++ Pay4MeAndroid/src/com/ebay/pay4me/android/Pay4MeAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.ebay.pay4me.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Pay4MeAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
